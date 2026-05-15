1e8bcb2bfaadbce8ddcfab6ab72b0ccd5d29a04b
iyanu9491-netizen <iyanu9491@gmail.com>
2026-05-15 20:33:00 +0100

    Implemented the forgotPasswod and Register Validation

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,8 @@
 const user = require('../model/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const {brevo} = require('../utils/brevo');
+const {emailTemplate, resetPasswordTemplate, resetPasswordSuccessfulTemplate}= require('../email');
 
 exports.register = async (req, res, next) => {
     try {
@@ -69,6 +71,33 @@ exports.login = async (req,res)=>{
             token,
             existingUser
         })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+exports.forgotPassword = async (req, res) => {
+    try {
+        const {email} = req.body
+        const existingUser = await user.findOne({email: email.toLowerCase()});
+        if(!existingUser) {
+            return res.status(400).json({
+                message: "User with this email does not exist"
+            })
+        }
+            const OTP = Math.round(Math.random() * 1e6).toString().padStart(6, '0');
+            existingUser.otp = OTP;
+            existingUser.otpExpires = Date.now() + (30 * 1000)
+            const data = {
+                name: `${existingUser.firstName} ${existingUser.lastName}`,
+                otp: OTP
+            }
+            await brevo(email, `${existingUser.firstName} ${existingUser.lastName}`, resetPasswordTemplate(data));
+            await existingUser.save();
+            res.status(200).json({
+                message: "Password reset OTP sent successfully"
+            });
     } catch (error) {
         res.status(500).json({
             message: error.message
